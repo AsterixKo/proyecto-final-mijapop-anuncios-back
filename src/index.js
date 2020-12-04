dfcb8e73728af6f406ae5f4fa1e05a63e707c4a2
MSI\moneo <jvr.moneo@gmail.com>
2020-12-04 17:34:01 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+//CONFIGURAMOS dotenv
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const pruebaRouter = require('./routes/pruebaRouter');
+const bookRouter = require('./routes/bookRouter');
+const authorRouter = require('./routes/authorRouter');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+mongoose.connect(
+    `mongodb+srv://${process.env.MATLAS_USER}:${process.env.MATLAS_PASS}@cluster0.xyydi.mongodb.net/mijapop?retryWrites=true&w=majority`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+//pruebas
+app.use('/books', bookRouter);
+app.use('/authors', authorRouter);
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h2>Llama a la ruta específica</h2>')
+});
+
+app.listen(3000, () => console.log('Aplicacion corriendo en el puerto 3000'));
